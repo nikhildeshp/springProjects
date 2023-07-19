616a5d479fb9872b34bcccea7ff31c727bd76843
nikhil deshpande <inikidesh@gmail.com>
2023-07-19 14:12:48 +0530

    employee management system

---
+++ 07-spring-boot-spring-mvc-crud-thymleaf/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	// load employee data
+
+   private EmployeeService employeeService;
+   @Autowired
+   EmployeeController(EmployeeService employeeService)
+   {
+	   this.employeeService=employeeService;
+   }
+
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+List<Employee> theEmployees=employeeService.findAll();
+
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
+	}
+}
+
+
+
+
+
+
+
+
+
